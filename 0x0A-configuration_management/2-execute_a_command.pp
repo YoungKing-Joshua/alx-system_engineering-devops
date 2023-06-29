b9d83d89a4fc49f2da7e230d12ca26534c72f121
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-06-29 04:16:28 +0000

    0x0A. Configuration management

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+# creating a manifest that kills a process named killmenow
+
+exec { 'killmenow':
+	command  => 'pkill -9 killmenow',
+	path     => '/usr/bin:/bin',
+	onlyif   => 'pgrep killmenow',
+	provider => shell,
+	refreshonly => true,
+}
