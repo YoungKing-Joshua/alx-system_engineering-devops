af3220dbe25936d3b8c9077b0c12631101dd1c05
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-07-25 05:10:43 +0000

    0x15. API

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script exports data into CSV format"""
+
+import csv
+import requests
+import sys
+
+def export_to_csv(user_id):
+    url = "https://jsonplaceholder.typicode.com/"
+    user_data = requests.get(url + "users/{}".format(user_id)).json()
+    username = user_data.get("username")
+    todo_data = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo_data:
+            csv_writer.writerow([user_id, username, task.get("completed"), task.get("title")])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Please provide a valid user ID as a command-line argument.")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    export_to_csv(user_id)
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script exports to-do list info of all employees to JSON format."""
+
+import json
+import requests
+
+def export_all_to_json():
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    all_tasks = {}
+    for user in users:
+        user_id = str(user["id"])
+        username = user["username"]
+        todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+        user_tasks = [
+            {
+                "username": username,
+                "task": task["title"],
+                "completed": task["completed"]
+            } for task in todos
+        ]
+        all_tasks[user_id] = user_tasks
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(all_tasks, jsonfile)
+
+if __name__ == "__main__":
+    export_all_to_json()
