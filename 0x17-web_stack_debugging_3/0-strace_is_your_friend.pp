a0fa9d64fa46717b554f7581d70e04668adfa943
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-08-13 14:37:24 +0000

    0x17. Web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+#Puppet config fix
+
+$php_config_file = '/var/www/html/wp-settings.php'
+
+exec { 'fix-php-config':
+  command     => "sed -i 's/phpp/php/g' ${php_config_file}",
+  path        => '/usr/local/bin/:/bin',
+  refreshonly => true,
+}
+
