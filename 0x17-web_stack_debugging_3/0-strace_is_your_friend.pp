28c7d92f22476654b5cee0ecb6363b089c8c3853
YoungKing-Joshua <joshuadeialorse@gmail.com>
2023-08-13 20:59:51 -0700

    0x17. Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -5,6 +5,5 @@ $php_config_file = '/var/www/html/wp-settings.php'
 exec { 'fix-php-config':
   command     => "sed -i 's/phpp/php/g' ${php_config_file}",
   path        => '/usr/local/bin/:/bin',
-  refreshonly => true,
 }
 
