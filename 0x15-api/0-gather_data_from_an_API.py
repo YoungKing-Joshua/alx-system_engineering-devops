1ff2df41a13b8104770d8114e33890867fc61903
YoungKing-Joshua <joshuadeialorse@gmail.com>
2024-01-04 18:20:14 -0800

    0x15. API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 
     # Display the result
     print("{} is done with tasks({}/{}):"
-          .format(client['name'], len(sks), tks))
+          .format(client['name'], len(sks), len(tdt)))
 
     # Display completed tasks
     for sk in sks:
